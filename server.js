2375fb2f35fb052e70db9f2b0c6f9dd0a12ece6e
Emmanuel Hudson <adjetey545@gmail.com>
2024-05-04 04:38:35 -0700

    added packages

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
+require("dotenv").config();
+
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const { errorHandler, notFound } = require("./handlers");
 
 const app = express();
 const port = 4000;
 // app.use(express.static('public'))
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use(express.json());
 
 app.get("/", (req, res) => res.send("Hello world"));
 
