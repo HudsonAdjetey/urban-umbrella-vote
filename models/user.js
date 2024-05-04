1f77e14c1c92366ef417fab01733eb7a5ae7d25b
Emmanuel Hudson <adjetey545@gmail.com>
2024-05-04 06:52:21 -0700

    salting password

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt")
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
@@ -12,6 +13,17 @@ const userSchema = new mongoose.Schema({
 });
 
 // creating the user
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password" || this.isNew )) return next();
+    try {
+        const salt = await bcrypt.genSalt(10)
+        this.password = await bcrypt.hash(this.password, salt)
+    } catch (error) {
+        next(error)
+    }
+    next()
+})
 
 const User = mongoose.model("Users", userSchema);
 
